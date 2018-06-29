d693058e84dea9015f76f60ea6a331952c79de18
nikesh-mahalka-2 <38130007+nikesh-mahalka-2@users.noreply.github.com>
2018-06-29 21:20:23 +0530

    Update check_health.sh

--- .cloudbuild/check_health.sh
+++ .cloudbuild/check_health.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-set -xe
-echo ${_DB_HLTH_CHK_MAX_RETRY}
-echo "${_DB_HLTH_CHK_MAX_RETRY}"
-num_retry=0
-until [[ "$num_retry" -gt "${_DB_HLTH_CHK_MAX_RETRY}" ]]
-do
-  echo "Oracle DB is $DB_HEALTH"
-  exit 0
-done
+ls lkdslk
+
+
